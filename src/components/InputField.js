74278995973b36b9e690857874ac6ba5ece8a562
ArbajAr7 <arbajrmacr786@gmail.com>
2024-07-19 21:50:48 +0530

    second commit

--- src/components/InputField.js
+++ src/components/InputField.js
@@ -2,13 +2,18 @@ import React from 'react';
 
 const InputField = (props) => {
   const { divid, nameid, ageid, submitbtn, removeField } = props;
-  
+
+  // Function to handle form submission and prevent default behavior
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+
   return (
-    <div id={divid}>
+    <form id={divid} onSubmit={handleSubmit}>
       <input id={nameid} type='text' placeholder='Sam'></input>
       <input id={ageid} type='text' placeholder='Age'></input>
-      <button id={submitbtn} onClick={() => removeField(divid)}>Remove</button>
-    </div>
+      <button id={submitbtn} type="button" onClick={() => removeField(divid)}>Remove</button>
+    </form>
   );
 }
 
